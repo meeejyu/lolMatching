937f9c325f379d9fd0dd1dfb30607091092ac86a
meeejyu <alwn04016@naver.com>
2022-12-04 23:39:23 +0900

    sqs 의존성 추가 sqs 의존성 추가 후 실행이 안되는 에러 픽스 필요함

--- matching/src/main/java/com/lol/matching/service/MainService.java
+++ matching/src/main/java/com/lol/matching/service/MainService.java
@@ -1,30 +1,20 @@
 package com.lol.matching.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
-// import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
-
-// import com.amazonaws.services.s3.internal.eventstreaming.Message;
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSAsync;
-// import com.amazonaws.services.sqs.model.Message;
 
 @Service
 public class MainService {
     
-    private final QueueMessagingTemplate queueMessagingTemplate;
+    // private final QueueMessagingTemplate queueMessagingTemplate;
 
-    @Autowired
-    public MainService(AmazonSQS amazonSQS) {
-        this.queueMessagingTemplate = new QueueMessagingTemplate((AmazonSQSAsync) amazonSQS);
-    }
+    // @Autowired
+    // public MainService(AmazonSQS amazonSQS) {
+    //     this.queueMessagingTemplate = new QueueMessagingTemplate((AmazonSQSAsync) amazonSQS);
+    // }
 
-    public void sendMessage(org.springframework.messaging.Message<?> message) {
-        // Message<?> newMessage = MessageBuilder.withPayload(message).build();
-        Message<?> newMessage = MessageBuilder.withPayload(message).build();
-        queueMessagingTemplate.send("보낼 메세지", message);
-    }
+    // public void sendMessage(org.springframework.messaging.Message<?> message) {
+    //     // Message<?> newMessage = MessageBuilder.withPayload(message).build();
+    //     Message<?> newMessage = MessageBuilder.withPayload(message).build();
+    //     queueMessagingTemplate.send("보낼 메세지", message);
+    // }
 }
