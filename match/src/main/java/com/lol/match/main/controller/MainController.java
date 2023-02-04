77a466b63a3a4987382a42b143f6abe05f44e9bc
meeejyu <alwn04016@naver.com>
2023-02-04 22:34:45 +0900

    feat : "mmr 및 포지션에 따른 팀 분배"

--- match/src/main/java/com/lol/match/main/controller/MainController.java
+++ match/src/main/java/com/lol/match/main/controller/MainController.java
@@ -2,6 +2,7 @@ package com.lol.match.main.controller;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -181,35 +182,161 @@ public class MainController {
         // 키 값 돌려서 포지션 알아내서 비교하기
         HashOperations<String, Object, Object> hashOperations = redisTemplate.opsForHash();
         
-        Map<Object, Object> map = hashOperations.entries("accept:Gold_520_620_f7fe6169-8f2c-4491-b0c6-6a8079e790f2");
+        Map<Object, Object> map = hashOperations.entries("map:Gold_520_620_c0dc9b5d-71e9-4cc2-aa03-5e31a6bfcaaa");
 
-        Map<String, String> topMap = new HashMap<>();
+        Map<Object, Integer> topMap = new HashMap<>();
+        List<Object> topList = new ArrayList<>();
 
-        Map<String, String> junglepMap = new HashMap<>();
+        Map<Object, Integer> junglepMap = new HashMap<>();
+        List<Object> jungleList = new ArrayList<>();
 
-        Map<String, String> midMap = new HashMap<>();
+        Map<Object, Integer> midMap = new HashMap<>();
+        List<Object> midList = new ArrayList<>();
 
-        Map<String, String> bottomMap = new HashMap<>();
+        Map<Object, Integer> bottomMap = new HashMap<>();
+        List<Object> bottomList = new ArrayList<>();
 
-        Map<String, String> supportMap = new HashMap<>();
+        Map<Object, Integer> supportMap = new HashMap<>();
+        List<Object> supportList = new ArrayList<>();
 
-        Map<String, String> aMap = new HashMap<>();
-
-        Map<String, String> bMap = new HashMap<>();
-
+        Map<Integer, List<String>> teamCom = new HashMap<>();        
 
         // postion이랑 mmr 얻어와서 비교하기
         for(Object key : map.keySet() ){
             UserMatchDto user = objectMapper.readValue(map.get(key).toString(), UserMatchDto.class);
             if(key.toString().contains("top")) {
-                // System.out.println(userMatchDto.toString());
-
+                topMap.put(key, user.getMmr());
+                topList.add(key);
+            }
+            else if(key.toString().contains("support")) {
+                supportMap.put(key, user.getMmr());
+                supportList.add(key);
+            }
+            else if(key.toString().contains("mid")) {
+                midMap.put(key, user.getMmr());
+                midList.add(key);
+            }
+            else if(key.toString().contains("jungle")) {
+                junglepMap.put(key, user.getMmr());
+                jungleList.add(key);
+            }
+            else if(key.toString().contains("bottom")) {
+                bottomMap.put(key, user.getMmr());
+                bottomList.add(key);
             }
         }
 
-        System.out.println(map.toString());
+        for (int i = 0; i < topMap.size(); i++) {
+            int topA = topMap.get(topList.get(i));
+            for (int j = 0; j < supportMap.size(); j++) {
+                int supportA = supportMap.get(supportList.get(j));    
+                for (int q = 0; q < midMap.size(); q++) {
+                    int midA = midMap.get(midList.get(q));    
+                    for (int e = 0; e < junglepMap.size(); e++) {
+                        int jungleA = junglepMap.get(jungleList.get(e));    
+                        for (int r = 0; r < bottomMap.size(); r++) {
+                            int bottomA = bottomMap.get(bottomList.get(r)); 
+                            int sumA = topA + supportA + midA + jungleA + bottomA;
+                            int topB = 0;
+                            int supportB = 0;
+                            int midB = 0;
+                            int jungleB = 0;
+                            int bottomB = 0;
+                            String aList = topList.get(i)+"/"+supportList.get(j)+"/"+midList.get(q)+"/"+jungleList.get(e)+"/"+bottomList.get(r);
+                            
+                            // 팀 B의 리스트 설정, 팀 A와 반대되는 유저 추가
+                            String bList = "";
+                            if(i==0) {
+                                topB = topMap.get(topList.get(1));
+                                bList += topList.get(1);
+                            }
+                            else if(i==1) {
+                                topB = topMap.get(topList.get(0));
+                                bList += topList.get(0);
+                            }
+                            if(j==0) {
+                                supportB = supportMap.get(supportList.get(1)); 
+                                bList += "/"+supportList.get(1);
+                            }
+                            else if(j==1) {
+                                supportB = supportMap.get(supportList.get(0)); 
+                                bList += "/"+supportList.get(0);
+                            }
+                            if(q==0) {
+                                midB = midMap.get(midList.get(1)); 
+                                bList += "/"+midList.get(1);
+                            }
+                            else if(q==1) {
+                                midB = midMap.get(midList.get(0)); 
+                                bList += "/"+midList.get(0);
+                            }
+                            if(e==0) {
+                                jungleB = junglepMap.get(jungleList.get(1));  
+                                bList += "/"+jungleList.get(1);
+                            }
+                            else if(e==1) {
+                                jungleB = junglepMap.get(jungleList.get(0));  
+                                bList += "/"+jungleList.get(0);
+                            }
+                            if(r==0) {
+                                bottomB = bottomMap.get(bottomList.get(1)); 
+                                bList += "/"+bottomList.get(1);
+                            }
+                            else if(r==1) {
+                                bottomB = bottomMap.get(bottomList.get(0)); 
+                                bList += "/"+bottomList.get(0);
+                            }
+                            int sumB = topB + supportB + midB + jungleB + bottomB;
+                            int sumDif = Math.abs(sumA - sumB);
+                            List<String> teamList = new ArrayList<>();
+                            
+                            teamList.add(aList);
+                            teamList.add(bList);
+                            if(sumDif==0) {
+                                teamCom.put(sumDif, teamList);
+                                break;
+                            }
+                            // else {
+                            if(teamCom.size() > 0) {
+                                for(Integer key : teamCom.keySet() ){
+                                    if(key > sumDif) {
+                                        teamCom.remove(key);
+                                        teamCom.put(sumDif, teamList);
+                                    }
+                                }
+                            }
+                            if(teamCom.size() == 0) {
+                                teamCom.put(sumDif, teamList);
+                            }
+                                
+                            // }
+                        }
+                    }
+                }
+            }    
+        }
+        
+        String[] aList = {};
+        String[] bList = {};
+        for(Integer key : teamCom.keySet() ){
+            System.out.println("mmr 차이 : "+key);
+            aList = teamCom.get(key).get(0).split("/");
+            bList = teamCom.get(key).get(1).split("/");
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띠용? : "+aList[i]);
+            // Object objectA = hashOperations.get("accept:Gold_520_620_f7fe6169-8f2c-4491-b0c6-6a8079e790f2", aList[i]);
+            // Object objectB = hashOperations.get("accept:Gold_520_620_f7fe6169-8f2c-4491-b0c6-6a8079e790f2", bList[i]);
+            Object objectA = hashOperations.get("map:Gold_520_620_c0dc9b5d-71e9-4cc2-aa03-5e31a6bfcaaa", aList[i]);
+            Object objectB = hashOperations.get("map:Gold_520_620_c0dc9b5d-71e9-4cc2-aa03-5e31a6bfcaaa", bList[i]);
+            hashOperations.put("teamA:Gold_520_620_c0dc9b5d-71e9-4cc2-aa03-5e31a6bfcaaa", aList[i], objectA);
+            hashOperations.put("teamB:Gold_520_620_c0dc9b5d-71e9-4cc2-aa03-5e31a6bfcaaa", bList[i], objectB);
+        }
 
         return "ok";
+    }
+
+    private void teamDivid(Map<String, Integer> bottomMap) {
     }
 
     @GetMapping("/test")
